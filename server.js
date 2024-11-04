2bc9f7300b392ee3598b62e9e57f81320f57dc1b
esanchez2898 <esanchez2898@outlook.com>
2024-11-04 17:19:47 -0600

    Backend working

--- server.js
+++ server.js
@@ -1,99 +1,150 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
+//1. Configuración Inicial
 
-const logEvents = require('./logEvents');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter { };
-// initialize object 
-const myEmitter = new Emitter();
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
-const PORT = process.env.PORT || 3500;
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3500
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(
-            filePath,
-            !contentType.includes('image') ? 'utf8' : ''
-        );
-        const data = contentType === 'application/json'
-            ? JSON.parse(rawData) : rawData;
-        response.writeHead(
-            filePath.includes('404.html') ? 404 : 200,
-            { 'Content-Type': contentType }
-        );
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-    } catch (err) {
-        console.log(err);
-        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
-        response.statusCode = 500;
-        response.end();
+
+
+
+// 2. Importando Middleware
+
+const { logger } = require('./middleware/logEvents')
+const credentials = require('./middleware/credentials')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+const cookieParser = require('cookie-parser')
+const verifyJWT = require('./middleware/verifyJWT')
+const errorHandler = require('./middleware/errorHandler')
+
+
+
+
+
+//3. Aplicación de Middlewares
+
+app.use(logger)                                             // Custom middleware to log each HTTP request to a log file                 // ?
+app.use(credentials);                                       // Primero, activa credenciales si el origen está permitido                 // ????
+app.use(cors(corsOptions));                                 // Luego, configura las opciones CORS para definir quién puede acceder      // ?????????
+
+app.use(express.urlencoded({ extended: false }))            // built-in middleware to handle urlcoded form data
+app.use(express.json())                                     // built-in middleware for json
+app.use(cookieParser())                                     // middleware for cookies
+
+app.use(express.static(path.join(__dirname, '/public')))    // Sirve archivos estáticos desde la carpeta 'public'
+//app.use('/subdir', express.static(path.join(__dirname, '/public')))
+
+
+
+
+
+
+// 4. Definiendo Middlewares de las Rutas
+
+app.use('/register', require('./routes/register'))
+app.use('/auth', require('./routes/auth'))
+app.use('/refresh', require('./routes/refresh'))
+app.use('/logout', require('./routes/logout'))
+//app.use('/subdir', require('./routes/subdir'))
+
+
+
+
+
+
+
+// 5. Protección de Rutas con Autenticación
+
+app.use(verifyJWT) // this is like a waterfall, so everithing after that it's going to be protected
+app.use('/', require('./routes/root'))
+app.use('/employees', require('./routes/api/employees'))
+
+
+
+
+
+
+// 6. Manejo de Errores para Rutas No Encontradas (404)
+
+app.all('*', (req, res) => { // all http methods at ones
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" })
+    } else {
+        res.type('txt').send("404 Not Found")
     }
+})
+
+
+
+
+
+
+// 7. Manejo de Errores
+
+app.use(errorHandler)
+
+
+
+
+
+
+// 8. Arranque del Servidor
+
+app.listen(PORT, () => console.log(`Running PORT ${PORT}`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// Routes handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html')
+    next()
+}, (req, res) => {
+    res.send('hello world')
+})
+
+
+
+//Chaining route handlers
+const one = (req, res, next) => {
+    console.log('one')
+    next()
 }
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+const two = (req, res, next) => {
+    console.log('two')
+    next()
+}
 
-    const extension = path.extname(req.url);
+const three = (req, res, next) => {
+    console.log('three')
+    res.send('finished')
+}
 
-    let contentType;
+app.get('/chain(.html)?', [one, two, three])
 
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
+*/
 
-    let filePath =
-        contentType === 'text/html' && req.url === '/'
-            ? path.join(__dirname, 'views', 'index.html')
-            : contentType === 'text/html' && req.url.slice(-1) === '/'
-                ? path.join(__dirname, 'views', req.url, 'index.html')
-                : contentType === 'text/html'
-                    ? path.join(__dirname, 'views', req.url)
-                    : path.join(__dirname, req.url);
-
-    // makes .html extension not required in the browser
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-    const fileExists = fs.existsSync(filePath);
-
-    if (fileExists) {
-        serveFile(filePath, contentType, res);
-    } else {
-        switch (path.parse(filePath).base) {
-            case 'old-page.html':
-                res.writeHead(301, { 'Location': '/new-page.html' });
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, { 'Location': '/' });
-                res.end();
-                break;
-            default:
-                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
-        }
-    }
-});
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Others routes
+/*app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})*/
